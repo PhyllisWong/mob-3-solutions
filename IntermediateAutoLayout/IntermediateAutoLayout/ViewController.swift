fccc68a3ae29ad02479d20aadf06722dd0623f16
Phyllis Wong <wong.phyllism@gmail.com>
2018-01-17 12:15:05 -0800

    remote repo

---
+++ IntermediateAutoLayout/IntermediateAutoLayout/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IntermediateAutoLayout
+//
+//  Created by Eliel A. Gordon on 10/30/17.
+//  Copyright © 2017 Phyllis Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Intermediate Auto-Layout"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
