c314b0e01444efa680c8119cbce50ada78a338e7
Phyllis Wong <wong.phyllism@gmail.com>
2018-01-17 12:47:09 -0800

    challenge one solved, change navigation bar color

--- IntermediateAutoLayout/IntermediateAutoLayout/ViewController.swift
+++ IntermediateAutoLayout/IntermediateAutoLayout/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.title = "Intermediate Auto-Layout"
+        self.view.backgroundColor = UIColor.gray
     }
 
     override func didReceiveMemoryWarning() {
